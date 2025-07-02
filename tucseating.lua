9b313bbed8e5fc8350b2c1a09447543dd84fddbd
Matthias Werner <matthias.werner@cs.tu-chemnitz.de>
2025-07-02 14:45:51 +0200

    format place text

--- tucseating.lua
+++ tucseating.lua
@@ -14,11 +14,12 @@ function Seat:new(o)
    self.__index = self
    return o
 end
-AllSeats={}
-SEATREMOVED  = 0
-SEATEMPTY    = 1
-SEATASSIGNED = 2
 
+AllSeats={}
+SEATREMOVED  = 1
+SEATEMPTY    = 2
+SEATASSIGNED = 3
+AISLE        = 4
 
 function initRec(rows, cols)
    local cid = 0
@@ -28,7 +29,7 @@ function initRec(rows, cols)
    for r = 1, rows do
       for c = 1, cols do
 	 cid = cid+1
-	 AllSeats[cid] = Seat.new({id=cid, row=r, col=c, x=c-1-cols/2, y=r-1.0, rot=0, kind=SEATEMPTY, label="x"})
+	 AllSeats[cid] = Seat.new({id=cid, row=r, col=c, x=c-(cols+1)/2, y=r-1.0, rot=0, kind=SEATEMPTY, label=tostring(cid)})
 	 -- if cid % 21 == 0 then
 	 --   tex.sprint("\\typeout{***** Try to remove *****",AllSeats[cid].kind,"}")
 	 ---   AllSeats[cid].kind = SEATASSIGND
@@ -37,20 +38,37 @@ function initRec(rows, cols)
       end
    end
 end
+function seatDim(w,h)
+   AllSeats["seatwidth"] = w
+   AllSeats["seatheight"] = h
+end
+
 function seatIds()
    for ndx, v in ipairs(AllSeats) do
       tex.sprint(tostring(ndx),",")
    end
 end
 function findSeatAt(y,x)
-   for ndx,s in ipairs(AllSeats) do
-      if s.col ~= nil and s.kind ~= SEATREMOVED then
-	 if s.col == x and s.row == y then
-	    return ndx
-	 end
-      end
+   if y<0 then
+      y = AllSeats["rows"] + y + 1
    end
-   return nil
+   if x<0 then
+      x = AllSeats["cols"] + x + 1
+   end
+   local ndx = (y-1)* AllSeats["cols"] + x
+   if AllSeats[ndx].kind ~= SEATREMOVED then
+   return ndx
+   else
+      return nil
+   end
+   --for ndx,s in ipairs(AllSeats) do SEATREMOVED then
+   --   if s.col ~= nil and s.kind ~= SEATREMOVED then
+   --	 if s.col == x and s.row == y then
+   --	    return ndx
+   --	 end
+   --   end
+   --end
+   --return nil
 end	 
 function assignSeat(ndx,l)
    if ndx ~= nil and AllSeats[ndx].kind == SEATEMPTY then
@@ -62,24 +80,25 @@ function assignSeatAt(y,x,l)
    assignSeat(findSeatAt(y,x),l)
 end
 function removeSeat(ndx)
-   tex.sprint("\\typeout{call remove seat ",ndx,"}")
    if ndx ~= nil then
       AllSeats[ndx].kind = SEATREMOVED
-      tex.sprint("\\typeout{remove seat ",ndx," at ",AllSeats[ndx].x,",",AllSeats[ndx].y,"}")
+      --tex.sprint("\\typeout{remove seat ",ndx," at ",AllSeats[ndx].col,",",AllSeats[ndx].row,"}")
    end
 end
 function removeSeatAt(y,x)
    removeSeat(findSeatAt(y,x))
 end
 function seatempty(s) 
-   local width = 100/AllSeats["cols"]
-   tex.sprint("\\node[rectangle,empty seat, minimum width=",0.9/AllSeats["cols"],"\\linewidth] at (",s.x*width,",",s.y,") {Xy};")
-   tex.sprint("\\node[empty label, minimum width=",0.9/AllSeats["cols"],"\\linewidth] at (",s.x*width,",",s.y,") {",s.id,"};")
+   local width = AllSeats["seatwidth"]
+   local height = AllSeats["seatheight"]
+   tex.sprint("\\node[rectangle,empty seat, minimum width=",width,", minimum height=",height,"] at (",s.x,",",s.y,") {Xy};")
+   tex.sprint("\\node[empty label, minimum width=",width,"] at (",s.x,",",s.y,") {\\tucsassignedlabelformat{",s.label,"}};")
 end
 function seatassigned(s) 
-   local width = 100/AllSeats["cols"]
-   tex.sprint("\\node[rectangle,assigned seat, minimum width=",0.9/AllSeats["cols"],"\\linewidth] at (",s.x*width,",",s.y,") {Xy};")
-   tex.sprint("\\node[assigned label,minimum width=",0.9/AllSeats["cols"],"\\linewidth] at (",s.x*width,",",s.y,") {",s.label,"};")
+   local width = AllSeats["seatwidth"]
+   local height = AllSeats["seatheight"]
+   tex.sprint("\\node[rectangle,assigned seat, minimum width=",width,", minimum height=",height,"] at (",s.x,",",s.y,") {Xy};")
+   tex.sprint("\\node[assigned label,minimum width=",width,"] at (",s.x,",",s.y,") {\\tucsassignedlabelformat{",s.label,"}};")
 end
 
 function seatremoved(s) 
@@ -90,11 +109,11 @@ end
 function drawSeats()
  local  case_tbl =
     {
-       [SEATREMOVED] = seatremoved,
-       [SEATEMPTY] = seatempty,
+       [SEATREMOVED]  = seatremoved,
+       [AISLE]        = seatremoved,
+       [SEATEMPTY]    = seatempty,
        [SEATASSIGNED] = seatassigned,
     }
-   AllSeats[5].kind = SEATASSIGNED
    for _ ,s in  ipairs(AllSeats) do
       if s.kind ~= nil then
 	 func = case_tbl[s.kind]
