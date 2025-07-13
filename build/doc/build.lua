32c171563eddf9bf900f71feeb2ba514094fe8a9
Matthias Werner <matthias.werner@informatik.tu-chemnitz.de>
2025-07-13 18:31:25 +0200

    user documentation: layout and seating scheme

---
+++ build/doc/build.lua
@@ -0,0 +1,18 @@
+-- Build configuration for tucseating
+module = "tucseating"
+
+unpackfiles = {}
+sourcefiles= {
+   "*.sty",
+   "*.tsr",
+   "*.lua"
+}
+installfiles= {
+   "*.sty",
+   "*.tsr",
+   "*.lua"
+}
+
+typesetfiles = {
+   "tucseating-doc-de.tex",
+}
